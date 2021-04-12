796a6012747688e272ee1bcb28989ff97ec56baa
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2021-04-12 17:49:10 -0400

    initial android build work

--- build.py
+++ build.py
@@ -25,7 +25,7 @@ def execute(command):
     return True
   return False
   
-def sync():
+def sync(root):
   command = []
   command.append("git")
   command.append("submodule")
@@ -42,19 +42,19 @@ def replace(root, script, pattern, replacement):
       if not (name.endswith(".hpp")):
         continue
       path = os.path.join(base, name)
-      handle = open(path, "rt")
-      lines = handle.readlines()
-      handle.close()
+      descriptor = open(path, "rt")
+      lines = descriptor.readlines()
+      descriptor.close()
       for line in lines:
         if (pattern in line):
           print(path)
-          handle = open(path, "rt")
-          content = handle.read()
-          handle.close()
+          descriptor = open(path, "rt")
+          content = descriptor.read()
+          descriptor.close()
           content = content.replace(pattern, replacement)
-          handle = open(path, "wt")
-          handle.write(content)
-          handle.close()
+          descriptor = open(path, "wt")
+          descriptor.write(content)
+          descriptor.close()
           return True
   return True
   
@@ -72,16 +72,25 @@ def unfix(root, script):
     return False
   return True
   
-def make(build, api):
+def make(build, api, system, parameters):
   cwd = os.getcwd()
   if not (os.path.isdir(build)):
     os.makedirs(build)
   command = []
   command.append("cmake")
   command.append("-G")
-  command.append("Visual Studio 15 2017 Win64")
+  if ("android" in system):
+    command.append("Ninja")
+  else:
+    if ("windows" in system):
+      command.append("Visual Studio 15 2017 Win64")
+    else:
+      command.append("Unix Makefiles")
   command.append("../..")
+  command.append("--debug-trycompile")
   command.append("-DGODOT_API_JSON="+api)
+  for parameter in parameters:
+    command.append(parameter)
   os.chdir(build)
   if not (execute(command)):
     return -1
@@ -94,27 +103,11 @@ def make(build, api):
     return -2
   return 0
   
-def run(root, target):
-  #print(target)
-  json = "godot_api.json"
-  if not ("GODOT_HOME" in os.environ):
-    return -1
-  home = os.environ["GODOT_HOME"]
-  #print(home)
-  godot = os.path.join(home, "Godot").replace("\\", "/")
-  #print(godot)
-  command = []
-  command.append(godot)
-  command.append("--gdnative-generate-json-api")
-  command.append(json)
-  if not (execute(command)):
-    return -2
-  #print(root)
-  api = os.path.join(root, json).replace("\\", "/")
-  build = os.path.join(root, "godot-cpp-cmake", "build", platform.system().lower()).replace("\\", "/")
-  if (target == "all"):
-    if not (make(build, api) == 0):
-      return -3
+def handle(root, target, api, system, parameters):
+  build = os.path.join(root, "godot-cpp-cmake", "build", system).replace("\\", "/")
+  if ((target == "all") or (((system in target) or (target in system)) and ("bindings" in target))):
+    if not (make(build, api, system, parameters) == 0):
+      return -1
   build = os.path.join(root, "godot-cpp-cmake", "lib")
   for base, folders, files in os.walk(build):
     if not (base == build):
@@ -122,21 +115,79 @@ def run(root, target):
         path = os.path.join(build, name)
         if not (os.path.isfile(path)):
           shutil.copy(os.path.join(base, name), path)
-  build = os.path.join(root, "build", platform.system().lower()).replace("\\", "/")
-  if not (make(build, api) == 0):
+  build = os.path.join(root, "build", system).replace("\\", "/")
+  if ((target == "all") or (((system in target) or (target in system)) and not ("bindings" in system))):
+    if not (make(build, api, system, parameters) == 0):
+      return -2
+  return 0
+  
+def run(root, target):
+  #print(target)
+  system = platform.system().lower()
+  json = "godot_api.json"
+  if not ("GODOT_HOME" in os.environ):
+    return -1
+  if not ("ANDROID_NDK_ROOT" in os.environ):
+    return -2
+  godot = os.path.join(os.environ["GODOT_HOME"], "Godot").replace("\\", "/")
+  ndk = os.environ["ANDROID_NDK_ROOT"].replace("\\", "/")
+  if not ((os.path.isfile(godot)) or (os.path.isfile(godot+".exe"))):
+    return -3
+  if not (os.path.isdir(ndk)):
     return -4
+  #print(godot)
+  command = []
+  command.append(godot)
+  command.append("--gdnative-generate-json-api")
+  command.append(json)
+  if not (execute(command)):
+    return -5
+  #print(root)
+  api = os.path.join(root, json).replace("\\", "/")
+  #print(api)
+  parameters = []
+  if ((target == "all") or (system in target)):
+    if not (handle(root, target, api, system, parameters) == 0):
+      return -6
+  androids = []
+  androids.append("arm64-v8a")
+  if ((target == "all") or ("android" in target)):
+    for android in androids:
+      parameters = []
+      parameters.append("-DCMAKE_SYSTEM_NAME=Android")
+      parameters.append("-DANDROID_ABI="+android)
+      parameters.append("-DANDROID_NDK_ROOT="+ndk)
+      if not (handle(root, target, api, "android_"+android, parameters) == 0):
+        return -7
   libraries = []
-  for base, folders, files in os.walk(os.path.join(root, "build")):
-    for name in files:
-      if ((name.endswith(".dll")) or (name.endswith(".so")) or (name.endswith(".dylib"))):
-        path = os.path.join(base, name)
-        libraries.append(path)
-        break
+  if ((target == "all") or (system in target)):
+    walk = os.path.join(root, "build", system)
+    if (os.path.isdir(walk)):
+      for base, folders, files in os.walk(walk):
+        for name in files:
+          if ((name.endswith(".dll")) or (name.endswith(".so")) or (name.endswith(".dylib"))):
+            path = os.path.join(base, name)
+            libraries.append([path, system])
+            break
+    else:
+      return -8
+  if ((target == "all") or ("android" in target)):
+    for android in androids:
+      walk = os.path.join(root, "build", "android_"+android)
+      if (os.path.isdir(walk)):
+        for base, folders, files in os.walk(walk):
+          for name in files:
+            if ((name.endswith(".dll")) or (name.endswith(".so")) or (name.endswith(".dylib"))):
+              path = os.path.join(base, name)
+              libraries.append([path, "android_"+android])
+              break
+      else:
+        return -9
   for library in libraries:
-    name = os.path.basename(library)
+    name = os.path.basename(library[0])
     if not (name.startswith("lib")):
       name = "lib"+name
-    shutil.copy(library, os.path.join(root, "bin", name))
+    shutil.copy(library[0], os.path.join(root, "bin", library[1], name))
   return 0
   
 def launch(arguments):
@@ -145,7 +196,7 @@ def launch(arguments):
   script = arguments[0]
   target = arguments[1]
   root = wd()
-  if not (sync()):
+  if not (sync(root)):
     return False
   if not (fix(root, script)):
     return False
--- readme.txt
+++ readme.txt
@@ -2,6 +2,7 @@
 Requirements:
 
  * Windows 10
+ * Android Studio (SDK & NDK)
  * Visual Studio 2017
  * CMake 3.10
  * Python 3
@@ -9,6 +10,15 @@ Requirements:
 
 Usage:
 
+ * Export the directory path containing the install Android NDK as an environment variabled named "ANDROID_NDK_ROOT"
  * Export the directory path containing the installed Godot executable as an environment variable named "GODOT_HOME"
  * Call the "build.bat" script from the terminal
  * Open the repo clone as a project in Godot and run the main scene
+
+Examples:
+
+ * python build.py all
+ * python build.py windows
+ * python build.py android
+ * python build.py windows-bindings
+ * python build.py android-bindings
